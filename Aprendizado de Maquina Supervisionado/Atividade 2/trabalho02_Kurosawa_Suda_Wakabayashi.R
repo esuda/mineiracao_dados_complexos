8cc73d1fe989ed157ef15112224ab285eab224b1
Daniel Noriaki Kurosawa <daniel.kurosawa@cornershopapp.com>
2021-09-10 18:16:49 -0300

    adicionando trabalho 2 supervisionado

--- Aprendizado de Maquina Supervisionado/Atividade 2/trabalho02_Kurosawa_Suda_Wakabayashi.R
+++ Aprendizado de Maquina Supervisionado/Atividade 2/trabalho02_Kurosawa_Suda_Wakabayashi.R
@@ -33,11 +33,28 @@
 
 # Vejam os exerc?cios 02 e 03 para ver o funcionamento 
 # de uma funcao similar a essa.
-#setwd("/Users/nkuros/Documents/mineiracao_dados_complexos/Aprendizado de Maquina Supervisionado/Atividade 1/")
+setwd("/Users/nkuros/Documents/mineiracao_dados_complexos/Aprendizado de Maquina Supervisionado/Atividade 2/")
 
 
 rm(list=ls())
 graphics.off()
+
+calculaMatrizConfusaoRelativa <- function(cm){
+    
+    # Aplicamos a transposi��o para garantir que a referencia
+    # fique nas linhas e a predicao nas colunas
+    cm_absolute = t(cm$table)
+    
+    # SEMPRE construam e reportem a matriz de confusao relativa!
+    cm_relative = cm_absolute
+    
+    cm_relative[1,1] = round(cm_absolute[1,1]/sum(cm_absolute[1,]), digits=2)
+    cm_relative[1,2] = round(cm_absolute[1,2]/sum(cm_absolute[1,]), digits=2)
+    cm_relative[2,1] = round(cm_absolute[2,1]/sum(cm_absolute[2,]), digits=2)
+    cm_relative[2,2] = round(cm_absolute[2,2]/sum(cm_absolute[2,]), digits=2)
+    
+    return(cm_relative)  
+}
 
 getHypothesis <- function(real_feature_names, categorical_feature_names=F, degree=3){
     
@@ -65,11 +82,30 @@ getHypothesis <- function(real_feature_names, categorical_feature_names=F, degre
     return(hypothesis)
 }
 
+getLoss <- function(y_true, y_pred){
+    y_true <- as.numeric(y_true) - 1
+    
+    totalLoss <- 0
+    eps <- 1e-9
+    # Recall: length(y_true) == length(y_pred)
+    # loss = (1-y)*log2(1 - p + eps)) + y*log(p + eps)
+    # eps is used for numerical stability, it is very close to 0.
+    # Supose we have y = 1 and p = 1 (perfect prediction), the loss (without eps)
+    # would be 0*log2(0) + 1*log(1). It would result in NaN
+    # because of 0*log2(0). With eps: 0*log2(1e-9) + 1*log(1 + 1e-9) 
+    for(i in 1:length(y_true)){
+        loss <- -1*((1 - y_true[i])*log2(1 - y_pred[i] + eps) + y_true[i]*log2(y_pred[i] + eps))
+        totalLoss <- totalLoss + loss
+    }
+    totalLoss <- totalLoss/(length(y_true))
+    return(totalLoss)
+}
+
 # Comandos que leem os conjuntos de treino e de validacao
 
-train_set <- read.csv("./training_set_air_quality.csv", stringsAsFactors=TRUE)
-val_set <- read.csv("./validation_set_air_quality.csv", stringsAsFactors=TRUE)
-test_set <- read.csv("./test_set_air_quality.csv", stringsAsFactors=TRUE)
+train_set <- read.csv("./proteins_training_set.csv", stringsAsFactors=TRUE)
+val_set <- read.csv("./proteins_validation_set.csv", stringsAsFactors=TRUE)
+# test_set <- read.csv("./test_set_air_quality.csv", stringsAsFactors=TRUE)
 
 # Desenvolvam o trabalho a partir daqui, apos executarem os comandos a cima
 
@@ -80,7 +116,9 @@ test_set <- read.csv("./test_set_air_quality.csv", stringsAsFactors=TRUE)
 library(ggplot2)
 library(dplyr)
 library(reshape2)
-
+library(glmnet)
+library(caret)
+library(pROC)
 #### Tarefas
 
 # 1.
@@ -88,29 +126,34 @@ library(reshape2)
 # Validacao se os conjuntos sao disjuntos
 ## Ok, todos os conjuntos sao disjuntos
 merge(train_set, val_set)
-merge(train_set, test_set)
-merge(test_set, val_set)
+#merge(train_set, test_set)
+#merge(test_set, val_set)
 
 # Validacao de volumetria das bases
-summary(train_set)
+summary()
+count(train_set[train_set$target == 0,])
+count(train_set[train_set$target == 1,])
 colnames(train_set)
 colnames(train_set)
-
+colSums()
+#percebemos que essas variáveis denotam em conjunto, a posição da proteina
+summary(train_set$end_position - train_set$start_position)
+ggplot(train_set$emini,
 summary(val_set)
 dim(val_set)
 colnames(val_set)
+count(val_set[val_set$target == 0,])
+count(val_set[val_set$target == 1,])
 
 summary(test_set)
 dim(test_set)
 colnames(test_set)
-
-#########
-# Verificacao da categoria marcada como (OTHER) na coluna wd
-tapply(train_set$No, train_set$wd, length)
+count(test_set[test_set$target == 0,])
+count(test_set[test_set$target == 1,])
 
 #########
 # Visualizacao da distribuição das variaveis
-melt_train_set <- train_set[, -which(names(train_set) %in% c('No', 'wd', 'year', 'month', 'day'))]
+melt_train_set <- train_set[,]
 melt_train_set <- melt(melt_train_set)
 
 p <- ggplot(data=melt_train_set, aes(x=value))+
@@ -118,70 +161,31 @@ p <- ggplot(data=melt_train_set, aes(x=value))+
     facet_wrap(~variable, scales='free'); p
 
 
-########
-# Visualizacao dos boxplots para retirada de outliers
-p <- ggplot(data = melt_train_set[melt_train_set$variable!='target', ], aes(x=variable, y=value)) + 
-    geom_boxplot()
-p + facet_wrap( ~ variable, scales="free_y"); p
-
-
-#########
-# Problema com a variavel RAIN, quase todos os registros tem valor 0
-ggplot(train_set, aes(x=RAIN)) + 
-    geom_histogram(color='White', bins=10) +
-    stat_bin(aes(label=..count..))
-
-tapply(train_set$No, train_set$RAIN, length)
-
-########
-# Funcao para criar Onehot encoding para a variavel de direcao do vento
-
-onehot_features <- function(dataset, feature){
-    cats <- unique(dataset[, feature])
-    for (cat in cats){
-        dataset[cat] <- as.numeric(dataset[,feature]==cat)
-    }
-    return(dataset)
-}
-train_set <- onehot_features(train_set, 'wd')
-val_set <- onehot_features(val_set, 'wd')
-test_set <- onehot_features(test_set, 'wd')
-
-#########
-# Retirando No e a última variável de vento  para gerar nosso caso BASELINE
-train_set_clean <- train_set[, -which(names(train_set) %in% c('No', 'wd', 'N'))]
-val_set_clean <- val_set[, -which(names(val_set) %in% c('No', 'wd', 'N'))]
-test_set_clean <- test_set[, -which(names(test_set) %in% c('No', 'wd', 'N'))]
-
-setdiff(colnames(train_set_clean), colnames(val_set_clean))
-setdiff(colnames(train_set_clean), colnames(test_set_clean))
-setdiff(colnames(val_set_clean), colnames(test_set_clean))
-
-summary(train_set_clean)
-
 #########
 # Devido a distribuicao nao normal dos dados iremos aplicar uma normalizacao min-max
 # exceto para as variaveis de data e a variavel target
-min_features <- apply(train_set_clean[,5:14], 2, min); min_features
-
-max_features <- apply(train_set_clean[,5:14], 2, max); max_features
+min_features <- apply(train_set[,1:(ncol(train_set)-1)], 2, min); min_features
+max_features <- apply(train_set[,1:(ncol(train_set)-1)], 2, max); max_features
 
 diff <- max_features - min_features; diff
 
-train_set_clean[,5:14] <- sweep(train_set_clean[,5:14], 2, min_features, "-")
-train_set_clean[,5:14] <- sweep(train_set_clean[,5:14], 2, diff, "/")
-summary(train_set_clean)
+train_set[,1:10] <- sweep(train_set[,1:(ncol(train_set)-1)], 2, min_features, "-")
+train_set[,1:10] <- sweep(train_set[,1:(ncol(train_set)-1)], 2, diff, "/")
+summary(train_set)
 
-val_set_clean[,5:14] <- sweep(val_set_clean[,5:14], 2, min_features, "-")
-val_set_clean[,5:14] <- sweep(val_set_clean[,5:14], 2, diff, "/")
+val_set[,1:10] <- sweep(val_set[,1:1(ncol(val_set)-1)], 2, min_features, "-")
+val_set[,1:10] <- sweep(val_set[,1:(ncol(val_set)-1)], 2, diff, "/")
 
-test_set_clean[,5:14] <- sweep(test_set_clean[,5:14], 2, min_features, "-")
-test_set_clean[,5:14] <- sweep(test_set_clean[,5:14], 2, diff, "/")
+#test_set[,] <- sweep(test_set[,1:(ncol(test_set)-1)], 2, min_features, "-")
+#test_set[,] <- sweep(test_set[,1:(ncol(test_set)-1)], 2, diff, "/")
 
-
+train_set$target <- as.factor(train_set$target)
+val_set$target <- as.factor(val_set$target)
+test_set$target <- as.factor(test_set$target)
+head(train_set)
 ########
 # Visualizacao dos dados apos normalizacao min max (Visualmente nao alterou muito a distribuicao)
-melt_train_set <- train_set_clean[, -which(names(train_set_clean) %in% c('No', 'wd', 'year', 'month', 'day', 'hour'))]
+melt_train_set <- val_set[,]
 melt_train_set <- melt(melt_train_set)
 
 ggplot(data=melt_train_set, aes(x=value))+
@@ -189,25 +193,113 @@ ggplot(data=melt_train_set, aes(x=value))+
     facet_wrap(~variable, scales='free')
 
 
-########
-# Criacao do modelo Baseline
-
-########
-# Para o onehot encoding, utilizar n-1 features de flags
-wd_columns <- c("NE","SE","SSE","SSW","NNE","SW","S","WNW","ESE","NNW","NW","W","E","ENE");wd_columns
-
-not_include <- c(wd_columns, 'target');not_include
-
-feature_names <- colnames(train_set_clean[, -which(names(train_set_clean) %in% not_include)]);feature_names
-
-
-hypothesis <- getHypothesis(feature_names, categorical_feature_names=wd_columns,degree=1);hypothesis
 
 ## Baseline ##
-baseline <- lm(formula=hypothesis, data=train_set_clean)
+# Treinando o Modelo - Classificação #
 
-valPred <- predict(baseline, val_set_clean)
-trainPred <- predict(baseline, train_set_clean)
+feature_names <- colnames(train_set)[1:(ncol(train_set)-1)]
+feature_names
+
+#gerando uma função com features a primeira potência para regressão logística
+hypothesis <- getHypothesis(feature_names, 1)
+hypothesis
+
+#help(glmnet) <- doc da regressão logística
+
+x_train <- model.matrix(hypothesis, train_set)
+y_train <- train_set$target
+
+
+model <- glmnet(x_train,
+                y_train, 
+                family="binomial", #descrição para regressão logística
+                standardize = FALSE,
+                maxit = 1e+05,
+                alpha=0, #tipo de regularização L2, 1 = l1
+                lambda = 1e-2
+                )
+
+### Verificando os thetas aprendidos ###
+model$beta
+model$a0 # valor do theta0 (intercept)
+
+#Previsões
+
+trainPred <- predict(model, newx = x_train, type="response")
+head(trainPred)
+
+
+#Convertendo para classes
+trainClassPred <- trainPred
+
+#Modificando para Limiar 0.5 , retorno de 0 ou 1
+threshold <- 0.5
+trainClassPred[trainPred >= threshold] <- 1
+trainClassPred[trainPred < threshold] <- 0
+
+trainClassPred
+
+#Valor de loss
+lossN = getLoss(train_set$target[train_set$target == 0], trainPred[train_set$target == 0])
+lossP = getLoss(train_set$target[train_set$target == 1], trainPred[train_set$target == 1])
+lossN
+lossP
+(lossN+lossP)/2
+
+
+#Matriz de confusão 
+
+cm <- confusionMatrix(data = as.factor(trainClassPred), 
+                      reference = as.factor(train_set$target), 
+                      positive='1')
+cm
+
+cm$table
+
+
+cm_relative <- calculaMatrizConfusaoRelativa(cm)
+cm_relative
+
+### Predicao no conjunto de validacao ###
+x_val <- model.matrix(hypothesis, valSet)
+y_val <- valSet$class
+valPred <- predict(model, newx = x_val, type="response")
+
+#valPred
+
+#converting to class
+valClassPred <- valPred
+
+
+#### THRESHOLD ####
+# Threshold = 0.5 
+valClassPred[valPred >= 0.5] <- 1
+valClassPred[valPred < 0.5] <- 0
+#matriz transposta relativa
+cm_relative <- calculaMatrizConfusaoRelativa(cm)
+cm_relative
+
+
+acc_bal_baseline <- (cm_relative[1,1] + cm_relative[2,2])/2
+acc_bal_baseline
+
+# ROC Curve for baseline
+ROC <- roc(val_set$target, val_set[,1], direction="<")
+ROC
+
+
+
+####################
+#### TO-DO
+#### 1- Combinação de variáveis
+#### 2- Balanceamento
+#### 3- Regressão logística Regularização
+
+
+baseline <- lm(formula=hypothesis, data=train_set)
+
+valPred <- predict(baseline, val_set)
+trainPred <- predict(baseline, train_set)
 
 MAE <- function(preds, labels){
     mae_values <- sum(abs(preds-labels))/length(preds)
@@ -226,22 +318,22 @@ R2 <- function(pred, true){
     return(r2)
 }
 
-mae_train_baseline <- MAE(trainPred, train_set_clean$target); mae_train_baseline
-mse_train_baseline <- MSE(trainPred, train_set_clean$target); mse_train_baseline
-r2_train_baseline <- R2(trainPred, train_set_clean$target);r2_train_baseline
+mae_train_baseline <- MAE(trainPred, train_set$target); mae_train_baseline
+mse_train_baseline <- MSE(trainPred, train_set$target); mse_train_baseline
+r2_train_baseline <- R2(trainPred, train_set$target);r2_train_baseline
 
 
-mae_val_baseline <- MAE(valPred, val_set_clean$target);mae_val_baseline
-mse_val_baseline <- MSE(valPred, val_set_clean$target);mse_val_baseline
-r2_val_baseline <- R2(valPred, val_set_clean$target);r2_val_baseline
+mae_val_baseline <- MAE(valPred, val_set$target);mae_val_baseline
+mse_val_baseline <- MSE(valPred, val_set$target);mse_val_baseline
+r2_val_baseline <- R2(valPred, val_set$target);r2_val_baseline
 
 
 # Retirando RAIN
 
-train_set_clean <- train_set_clean[, -which(names(train_set_clean) %in% c('RAIN'))];colnames(train_set_clean)
-val_set_clean <- val_set_clean[, -which(names(val_set_clean) %in% c('RAIN'))]
-test_set_clean <- test_set_clean[, -which(names(test_set_clean) %in% c('RAIN'))]
-feature_names <- colnames(train_set_clean[, -which(names(train_set_clean) %in% not_include)]);feature_names
+train_set <- train_set[, -which(names(train_set) %in% c('RAIN'))];colnames(train_set)
+val_set <- val_set[, -which(names(val_set) %in% c('RAIN'))]
+test_set <- test_set[, -which(names(test_set) %in% c('RAIN'))]
+feature_names <- colnames(train_set[, -which(names(train_set) %in% not_include)]);feature_names
 
 ########
 # Criacao de modelos atraves da combinacao de features
@@ -304,8 +396,8 @@ total_r2_val_noCat <- c(length(modelsCategorical))
 
 
 i <- 1
-dataTrain <- train_set_clean
-dataVal <- val_set_clean
+dataTrain <- train_set
+dataVal <- val_set
 
 
 for(f in modelsCategorical){
@@ -448,10 +540,40 @@ legend(0.7, y=NULL,
 ######## A PARTIR DAQUI CALCULOS DE ERRO DO TEST SET ##########
 model <- lm(formula=h04, data=dataTest)
 
-testPred <- predict(model, test_set_clean)
-mae_test <- MAE(testPred, test_set_clean$target);mae_test
+testPred <- predict(model, test_set)
+mae_test <- MAE(testPred, test_set$target);mae_test
 
-mse_test <- MSE(testPred, test_set_clean$target);mse_test
+mse_test <- MSE(testPred, test_set$target);mse_test
 
-r2_test <- R2(testPred, test_set_clean$target);r2_test
+r2_test <- R2(testPred, test_set$target);r2_test
 
+
+
+
+
+
+
+###############################################################################################################################
+
+
+mean_features <- apply(trainSet[,1:(ncol(trainSet)-1)], 2, mean)
+mean_features
+
+
+sd_features <- apply(trainSet[,1:(ncol(trainSet)-1)], 2, sd)
+sd_features
+
+trainSet[,1:(ncol(trainSet)-1)] <- sweep(trainSet[,1:(ncol(trainSet)-1)], 2, mean_features, "-")
+trainSet[,1:(ncol(trainSet)-1)] <- sweep(trainSet[,1:(ncol(trainSet)-1)], 2, sd_features, "/")
+summary(trainSet)
+
+
+
+valSet[,1:(ncol(valSet)-1)] <- sweep(valSet[,1:(ncol(valSet)-1)], 2, mean_features, "-")
+valSet[,1:(ncol(valSet)-1)] <- sweep(valSet[,1:(ncol(valSet)-1)], 2, sd_features, "/")
+summary(valSet)
+
+
+testSet[,1:(ncol(testSet)-1)] <- sweep(testSet[,1:(ncol(testSet)-1)], 2, mean_features, "-")
+testSet[,1:(ncol(testSet)-1)] <- sweep(testSet[,1:(ncol(testSet)-1)], 2, sd_features, "/")
+summary(testSet)
